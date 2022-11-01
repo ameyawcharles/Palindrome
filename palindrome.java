e498856b0a7e6e800e9e6940fd58e14d51f15f2e
ameyawcharles <icharlesame@gmail.com>
2022-11-01 09:02:24 +0000

    First Commit

---
+++ palindrome.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args ) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a string to check if it's a palindrome");
+
+        original = in.nextLine();
+        int lenght = original.length();
+
+        for(int i = lenght - 1; i > = 0; i-)
+        reverse = reverse = original.charAt(i);
+        if(original.equals(reverse))
+         System.out.println("The string is a palindrome");
+
+        else
+
+        System.out.println("The string isn't a palindrome");
+
+
+
+    
+
+    }
+}
